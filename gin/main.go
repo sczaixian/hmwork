f7ac7151a65dcb8b374169e0f85a311e37624d60
c1911 <676825173@qq.com>
2025-08-10 22:36:53 +0800

    update;

--- gin/main.go
+++ gin/main.go
@@ -11,4 +11,26 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "hello world!!")
 	})
+	r.Any("/login", func(c *gin.Context) {
+		c.String(http.StatusOK, "any hello world!!")
+	})
+
+	grp1 := r.Group("/v1")
+	{
+		grp1.GET("/user", func(c *gin.Context) {
+			c.String(http.StatusOK, "v1 get user")
+		})
+	}
+
+	grp2 := r.Group("/v2")
+	{
+		grp2.GET("/user", func(c *gin.Context) {
+			c.String(http.StatusOK, "v2 get user")
+		})
+	}
+
+	err := r.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
